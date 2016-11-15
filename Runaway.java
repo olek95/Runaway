69f504677cf2a43de54fdf2828daed93ac1d53fa
AleksanderSklorz <olek1995s@poczta.onet.pl>
2016-11-15 22:52:34 +0100

    Update Runaway.java
    poprawienie granic uciekania i przesuwania

--- Runaway.java
+++ Runaway.java
@@ -2,6 +2,7 @@ package runaway;
 
 import java.util.Random;
 import javafx.application.Application;
+import javafx.geometry.Bounds;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
@@ -14,25 +15,22 @@ public class Runaway extends Application {
         Button runawayButton = new Button("Uciekinier");
         StackPane root = new StackPane();
         root.getChildren().add(runawayButton);
-        runawayButton.setOnMouseMoved(event -> {
-            System.out.println(runawayButton.getBoundsInParent().getMinX());
-            runawayButton.setTranslateX(rand.nextInt(300) - 150);
-            runawayButton.setTranslateY(rand.nextInt(250) - 125);
-            System.out.println(runawayButton.getBoundsInParent().getMinX());
-            runawayButton.relocate(runawayButton.getBoundsInParent().getMinX(),0);
-        });
         Scene scene = new Scene(root, 300, 250);
         
         primaryStage.setTitle("Program z uciekającym przyciskiem");
         primaryStage.setScene(scene);
         primaryStage.show();
+        runawayButton.setOnMouseMoved(event -> {
+            Bounds buttonBounds;
+            int sceneWidth = (int)scene.getWidth(), sceneHeight = (int)scene.getHeight(); 
+            do{
+                runawayButton.setTranslateX(rand.nextInt(sceneWidth) - sceneWidth/2);
+                runawayButton.setTranslateY(rand.nextInt(sceneHeight) - sceneHeight/2);
+                buttonBounds = runawayButton.getBoundsInParent();
+            }while(buttonBounds.getMinX() < 0 || buttonBounds.getMaxX() > sceneWidth || buttonBounds.getMinY() < 0 || buttonBounds.getMaxY() > sceneHeight);
+        });
     }
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         launch(args);
     }
-    
 }
